4272aab82f224a88a7132a66fc0516bf93e1772a
Ivan Simonini <ivan.simonini@roundhousecode.com>
2026-07-12 15:50:33 +0200

    Structure

--- game-03.js
+++ game-03.js
@@ -1,25 +1,31 @@
 console.log('game-03.js:start');
 
+var load_game_data = () => {
+	return {
+		title: 'Il Giochino'
+	};
+}
+var load_player_data = () => {
+	return {
+		name: 'GODS',
+		points: 1337
+	};
+}
 var load_game = (game_data, player_data) => {
 	console.log('load_game.start');
-	console.log('game_data.length=' + (game_data.length || 0));
 
-	document.title = game_data.title || 'Game Loaded';
-	$('#profile').html(player_data.name || '<NO INFO>');
+	document.title = game_data?.title || 'Game Loaded';
+	set_player_name(player_data?.name);
 	set_player_points(player_data?.points);
 
 	console.log('load_game.stop');
 }
+var set_player_name = (name) => { $('#profile').html(name || '<NO INFO>'); }
 var set_player_points = (points) => { $('#points').html((parseInt(points) || 0) + 'pts'); }
 
 $(() => {
-	var game_data = {
-		title: 'Il Giochino'
-	};
-	var player_data = {
-		name: 'GODS',
-		points: 1337
-	};
+	var game_data = load_game_data();
+	var player_data = load_player_data();
 	load_game(game_data, player_data);
 });
 
