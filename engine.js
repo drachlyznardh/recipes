04aa1b48810f3254c0b8cd611dd61bc948ab6ce0
Ivan Simonini <ivan.simonini@roundhousecode.com>
2026-07-19 20:25:21 +0200

    Maybe better

--- engine.js
+++ engine.js
@@ -177,7 +177,8 @@ var win = () => {
 	// // $('.victory.condition').html('You won!').attr('title', '').tooltip({ content: table.clone() });
 	// $('.victory.condition').html('You won!').attr('title', table.clone());
 	// $('.victory.condition').html('You won!').tooltip({ items: '.victory.condition' }).tooltip({ content: table.clone() });
-	$('.victory.condition').addClass('hover').on('click', () => $('.victory.recap').dialog('open'));
+	$('.victory.condition').val('You won!').attr('title', 'You won!').on('click', () => $('.victory.recap').dialog('open'));
+	// $('.victory.condition').addClass('hover').on('click', () => $('.victory.recap').dialog('open'));
 }
 var time_desc = (timems) => {
 	if (timems < 1000) return 'less than a second';
@@ -212,7 +213,8 @@ var check_amount = (amount) => {
 		// // $('.victory.condition').html(desc).attr('title', '').tooltip({ content: desc });
 		// $('.victory.condition').html(desc).attr('title', desc).tooltip({ items: '[title]' });
 		// $('.victory.condition').html(desc).tooltip({ items: '.victory.condition' }).tooltip({ content: desc });
-		$('.victory.condition').html(desc).removeClass('hover').attr('title', desc); // .tooltip({ items: '[title]' });
+		$('.victory.condition').val(desc).attr('title', desc); // .tooltip({ items: '[title]' });
+		// $('.victory.condition').html(desc).removeClass('hover').attr('title', desc); // .tooltip({ items: '[title]' });
 		return (storage) => { return amount?.every(e => storage[e[0]] >= e[1]); }
 	}
 }
