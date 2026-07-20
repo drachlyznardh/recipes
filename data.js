ee8b3a51faf2b93ca56b206b4cc468ac96adcfa7
Ivan Simonini <ivan.simonini@roundhousecode.com>
2026-07-20 18:15:42 +0200

    Nesting a bit

--- data.js
+++ data.js
@@ -1,25 +1,31 @@
 
 const load_game_data = () => {
+	const mk_about = () => {
+		const title = 'Recipes';
+		const version = '0.0.6';
+		const is_release = false;
+		const devel_suffix = '-dev';
+		const base_url = 'https://github.com/drachlyznardh';
+		const project_name = 'recipes';
+
+		const mklink = (href, name) => `<a href="${href}" target="_blank">${name}</a>`;
+		const full_version = is_release ? version : version + devel_suffix;
+		const version_link = mklink( //
+			`${base_url}/${project_name}/${is_release ? 'releases/tag/v' + full_version : 'tree/dev'}`, //
+			full_version);
+		const game_link = mklink(`${base_url}/${project_name}`, title);
+		const author_link = mklink(base_url, 'DrachLyznardh');
+		return [title, //
+			`v${full_version}`, //
+			`<p>This is version ${version_link} of ${game_link}</p>
+				<p>Written by ${author_link} for fun since 2026</p>`];
+	}
 	const mk_res = (name, key) => { return { name: name, key: key || name.toLowerCase() }; }
 	const mk_ress = (names) => { return names.map(mk_res); }
-	const title = 'Recipes';
-	const version = '0.0.6';
-	const is_release = true; // false;
-	const devel_suffix = '-dev';
-	const base_url = 'https://github.com/drachlyznardh';
-	const project_name = 'recipes';
-	const mklink = (href, name) => `<a href="${href}" target="_blank">${name}</a>`;
-	const full_version = is_release ? version : version + devel_suffix;
-	const version_link = mklink( //
-		`${base_url}/${project_name}/${is_release ? 'tags/v' + full_version : 'tree/dev'}`, //
-		full_version);
-	const game_link = mklink(`${base_url}/${project_name}`, title);
-	const author_link = mklink(base_url, 'DrachLyznardh');
-	const about = `<p>This is version ${version_link} of ${game_link}</p>
-		<p>Written by ${author_link} for fun since 2026</p>`;
+	[title, version, about] = mk_about();
 	return {
 		title: title,
-		version: `v${full_version}`,
+		version: version,
 		about: about,
 		autoresume: true,
 		scenarios: [{
