84eeefbdce03d364d63f9d7deac5282fa67fb694
Ivan Simonini <ivan.simonini@roundhousecode.com>
2026-07-21 18:29:14 +0200

    This may give you enough time

--- data.js
+++ data.js
@@ -62,12 +62,13 @@ const load_game_data = () => {
 			victory: check_amount([[0, 600]])
 		}, {
 			name: 'To the max!',
-			resources: mk_ress(['Dirt', 'Gold']),
+			resources: mk_ress(['Dirt', 'Gold', 'Glory']),
 			autoenable: true,
 			stepper: [{ name: 'Free dirt', product: [[0, 1]]},
-				{ name: 'Gold traces', req: [[0, 5]], product: [[1, 1]], max: [[1, 5]] },
-				{ name: 'Gold chunks', req: [[0, 25]], product: [[1, 5]] }],
-			victory: check_amount([[1, 10]])
+				{ name: 'Gold traces', req: [[0, 5]], product: [[1, 1]], max: [[1, 10]] },
+				{ name: 'Glory', req: [[0, 10]], product: [[2, 1]], max: [[2, 5]] },
+				{ name: 'Gold chunks', req: [[0, 50]], product: [[1, 10]] }],
+			victory: check_amount([[1, 20], [2, 5]])
 		}]
 	};
 }
