b6dd733f93d44b08fdad705312c0165ee52367f1
Ivan Simonini <ivan.simonini@roundhousecode.com>
2026-08-10 20:34:47 +0200

    Ok, this works

--- common/formatting.js
+++ common/formatting.js
@@ -3,7 +3,7 @@ function format(value) {
 	const v = value || 0;
 	if (v < 1) return '0';
 	const o = Math.min(PREFIXES.length, Math.floor(Math.floor(Math.log10(v)) / 3));
-	if (o == 0) return v.toString();
+	if (o == 0) return Math.floor(v.toString());
 	const p = PREFIXES[o - 1] || '';
 	const d = 10 ** (3 * o);
 	const e = v/d;
