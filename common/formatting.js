52598825659be7832ff90b25bd038f742f4409f0
Ivan Simonini <ivan.simonini@roundhousecode.com>
2026-08-10 21:39:48 +0200

    There, I broke it

--- common/formatting.js
+++ common/formatting.js
@@ -5,12 +5,12 @@ function format(value) {
 	const v = value || 0;
 	if (v < 1) return '0';
 	const o = Math.min(PREFIXES.length, Math.floor(Math.floor(Math.log10(v)) / 3));
-	if (o == 0) return Math.floor(v.toString());
+	if (o == 0) return Math.floor(v).toString();
 	const p = PREFIXES[o - 1] || '';
 	const d = 10 ** (3 * o);
 	const e = v/d;
 	if (e >= 10000) return 'Too much';
-	const f = e >= 1000 ? 10000 : 10 ** (2 - Math.floor(Math.log10(e)));
-	return `${Math.floor(e * f) / f}${p}`;
+	const f = e >= 1000 ? 10000 : Math.max(1, 10 ** (2 - Math.floor(Math.log10(e))));
+	return `${Math.floor(e / f) * f}${p}`;
 }
 
---
+++ number-format/engine.js
@@ -0,0 +1,39 @@
+
+const FPS = 10;
+
+var value = 0;
+var increment = 1;
+var round = 0;
+
+function nextStep() {
+	setTimeout(step, 1000 / FPS);
+}
+
+function setup() {
+	function mp12(v) { return [v - 2, v - 1, v, v + 1, v + 2]; }
+	function dm12(v) { return [v * .99, v * .999, v, v * 1.01, v * 1.1]; }
+	var values = [-2, -1, 0, .01, .1] //
+		.concat(Array.from(Array(22)).map((e, i) => i + 1)) //
+		.concat(Array.from(Array(4)).map((e, i) => 10 ** (i + 2)).map(mp12).flat()) //
+		.concat(Array.from(Array(9)).map((e, i) => 10 ** (i + 6)).map(dm12).flat()) //
+		.concat(Array.from(Array(8)).map((e, i) => 10 ** (3 * (i + 5)))) //
+		.concat(Array.from(Array(11)).map((e, i) => 10 ** 33 * (9990 + i)));
+	$('#value3').html(values.map(format).map(v => `<li>${v}</li>`));
+}
+
+function step() {
+	round++;
+	increment *= 1.1;
+	value += increment;
+
+	$('#value').html(`Value: ${format(value)} (${value})`);
+	$('#value2').html(`Increment: ${format(increment)} (${increment})`);
+	// $('#value3').html(value.toPrecision(3));
+	//  $('#value4').html(value.toPrecision(4));
+
+	nextStep();
+}
+
+$(setup);
+$(nextStep);
+
