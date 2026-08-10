c960f9fec3b9de06e7bb799952b51bdd2a4d825b
Ivan Simonini <ivan.simonini@roundhousecode.com>
2026-08-10 20:19:58 +0200

    Bad

---
+++ common/formatting.js
@@ -0,0 +1,14 @@
+
+export function format(value) {
+	const v = value || 0;
+	if (v < 1) return '0';
+	const o = Math.min(PREFIXES.length, Math.floor(Math.floor(Math.log10(v)) / 3));
+	if (o == 0) return v.toString();
+	const p = PREFIXES[o - 1] || '';
+	const d = 10 ** (3 * o);
+	const e = v/d;
+	if (e >= 10000) return 'Too much';
+	const f = e >= 1000 ? 10000 : 10 ** (2 - Math.floor(Math.log10(e)));
+	return `${Math.floor(e * f) / f}${p}`;
+}
+
