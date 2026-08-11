aeb9d2379169e0532b997cec16e53a037398989a
Ivan Simonini <ivan.simonini@roundhousecode.com>
2026-08-11 20:46:24 +0200

    Better

--- exp-and-power-ups/engine.js
+++ exp-and-power-ups/engine.js
@@ -1,43 +1,55 @@
 
 const FPS = 10;
-const MULTIPLIERS = 6;
+const AUTOBUY = true;
 
 var scenario = 0;
 var objective = 0;
 var total = 0;
 var available = 0;
 var round = 0;
-var increments = Array.from(Array(MULTIPLIERS)).map(e => 1);
-var cost = Array.from(Array(MULTIPLIERS)).map(e => 1);
+var increments = [];
+var cost = [];
 
-function checkVictory() { return objective >= available; }
+function checkVictory() { return objective <= available; }
 
 function setup() {
 	$('#victory').dialog({
 		modal: true,
 		width: 400,
 		resizable: false,
-		autoOpen: false
+		autoOpen: false,
+		title: 'Victory!'
 	});
-
-	objective = 10 ** (scenario * 2 + 6);
-	$('#objective').html(`Reach ${format(objective)} EXP to achieve victory`);
 
 	reset();
 }
 
+function nextScenario() {
+	scenario++;
+	reset();
+	$('#victory').dialog('close');
+}
+
 function reset() {
+	objective = 10 ** (scenario * 3 + 6);
+	$('#objective').html(`Reach ${format(objective)} EXP to achieve victory`);
+
 	size = 4 + scenario * 2;
 
 	cost = Array.from(Array(size)).map((e, i) => 10 ** (i + 1));
 
+	$('#victory').html(`<div class="center">
+			<div>You won!</div><input type="button" value="Play next scenario" onclick="nextScenario()" />
+		</div>`);
+
 	const div = $('#multipliers');
+	div.html('');
 	Array.from(Array(size)).map((e, i) => i) //
 		.map(e => `<div id="multi-${e}" class="multiplier center">
 				<div class="ui-widget ui-widget-content">
 					<div class="value">Value <span class="value">${format(increments[e])}</span></div>
 					<div class="cost">Cost <span class="cost">${format(cost[e])}</span></div>
-					<div class="multi-${e}"><input type="button" value="More" index="${e}"/></div>
+					<div class="multi-${e}"><input type="button" class="buyable" value="More" index="${e}"/></div>
 				</div>
 			</div>`) //
 		.map(e => $(e).appendTo(div));
@@ -64,7 +76,9 @@ function step() {
 	$('#increment span.increment').html(increments.map(format).join(' * ') + ' = ' + format(increment));
 	total += increment; $('#total span.total').html(format(total));
 	available += increment; $('#available span.available').html(format(available));
-	$('input[type=button]').each((j, e) => { $(e).button({ disabled: cost[parseInt($(e).attr('index'))] > available }); });
+	$('input.buyable').each((j, e) => { $(e).button({ disabled: cost[parseInt($(e).attr('index'))] > available }); });
+
+	if (AUTOBUY) $('input.buyable:enabled').click();
 
 	if (checkVictory()) $('#victory').dialog('open');
 	else nextStep();
