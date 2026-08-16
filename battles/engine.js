441cb70ae4397c036fd7f9ee9784ca0ca2eddee7
Ivan Simonini <ivan.simonini@roundhousecode.com>
2026-08-16 18:11:01 +0200

    Splitting

--- battles/engine.js
+++ battles/engine.js
@@ -1,24 +1,34 @@
 
 const TITLE = 'Battles'
 
+function setupTabs(list, container, active) {
+	const ul = $('<ul>').appendTo(container);
+	list.map(e => {
+		const selector = list.length > 2 ? e[2] : `tab-${e[0].toLowerCase()}`;
+		const description = e[0].startsWith('icon:') ? `<span class="ui-icon ui-icon-${e[0].split(':')[1]}"></span>` : e[0];
+		$(`<li title="${e[1]}"><a href="#${selector}">${description}</a></li>`).appendTo(ul);
+		$('<div>').prop('id', selector).appendTo(container);
+	});
+	container.tabs({ active: active || 0, classes: { 'ui-tabs-nav': 'ui-corner-top' } });
+}
+
+function setupOptions(container) {
+	const div = $('<div>').addClass(['ui-widget', 'ui-widget-content', 'ui-corner-all']) //
+		.appendTo($('<div>').addClass(['level']).appendTo(container));
+	$('<div>').html('Options').addClass(['ui-widget-header', 'ui-corner-top']).appendTo(div);
+}
+
 function setup() {
 	$('title').html(TITLE);
 
 	const main = $('<div>').addClass('main').appendTo($('body'));
-	const tabs = $('<div>').appendTo(main);
-	const ul = $('<ul>').appendTo(tabs);
-	[
-		['icon:gear', 'Options'],
+	setupTabs([
+		['icon:gear', 'Options', 'options'],
 		['Battle', 'Battle enemies and gain experience'],
 		['Experience', 'Spend experience and gain levels'],
 		['This', 'Helper text'],
 		['That', 'Helper text'],
-	].map(e => {
-		const selector = `tab-${e[0].toLowerCase()}`;
-		const description = e[0].startsWith('icon:') ? `<span class="ui-icon ui-icon-${e[0].split(':')[1]}"></span>` : e[0];
-		$(`<li title="${e[1]}"><a href="#${selector}">${description}</a></li>`).appendTo(ul);
-		$('<div>').prop('id', selector).appendTo(tabs);
-	});
-	tabs.tabs({ active: 1, classes: { 'ui-tabs-nav': 'ui-corner-top' } });
+	], $('<div>').appendTo(main), 0);
+	setupOptions($('#tab-options'));
 }
 
